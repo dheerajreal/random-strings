0eb62d4eeb317d00ae78f79c5604a47824e49657
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2021-07-05 15:32:02 +0530

    added more tests

--- test_random_strings.py
+++ test_random_strings.py
@@ -5,25 +5,64 @@ from random_strings import get_random_string
 
 
 class TestRandomStrings(unittest.TestCase):
-    def _string_lengths(self, length):
-        string = get_random_string(length)
-        match = re.fullmatch(r"^[a-zA-Z0-9]+$", string)
+    UPPER_LOWER_DIGIT_REGEX = r"^[A-Za-z0-9]+$"
+    NOUPPER_LOWER_DIGIT_REGEX = r"^[a-z0-9]+$"
+    UPPER_NOLOWER_DIGIT_REGEX = r"^[A-Z0-9]+$"
+    UPPER_LOWER_NODIGIT_REGEX = r"^[A-Za-z]+$"
+    NOUPPER_NOLOWER_DIGIT_REGEX = r"^[0-9]+$"
+    NOUPPER_LOWER_NODIGIT_REGEX = r"^[a-z]+$"
+    UPPER_NOLOWER_NODIGIT_REGEX = r"^[A-Z]+$"
+    valid_lengths = [8, 16, 32, 64, 512, 2048, True]  # True returns length 1
+    invalid_lengths = [0, -5, "hello", 3.14, {}, None, False]
+
+    def _string_lengths(self, length, *args, regex="", **kwargs):
+        string = get_random_string(length, *args, **kwargs)
+        match = re.fullmatch(regex, string)
         self.assertIsNotNone(match)
         self.assertEqual(len(string), length)
 
-    def test_small_string(self):
-        self._string_lengths(12)
+    def _invalid_string_lengths(self, length, *args, regex="", **kwargs):
+        with self.assertRaises(ValueError):
+            self._string_lengths(length, *args, regex=regex, **kwargs)
 
-    def test_medium_string(self):
-        self._string_lengths(32)
+    def test_valid_string_lengths(self):
+        for length in self.valid_lengths:
+            self._string_lengths(length, regex=self.UPPER_LOWER_DIGIT_REGEX)
 
-    def test_long_string(self):
-        self._string_lengths(512)
+    def test_invalid_string_lengths(self):
+        for length in self.invalid_lengths:
+            self._invalid_string_lengths(
+                length, regex=self.UPPER_LOWER_DIGIT_REGEX)
 
-    def test_invalid_length(self):
-        for i in [0, -5, None, "hello", 3.14]:
-            with self.assertRaises(ValueError):
-                self._string_lengths(i)
+    def test_upper_false(self):
+        for length in self.valid_lengths:
+            self._string_lengths(
+                length, regex=self.NOUPPER_LOWER_DIGIT_REGEX, upper=False)
+
+    def test_lower_false(self):
+        for length in self.valid_lengths:
+            self._string_lengths(
+                length, regex=self.UPPER_NOLOWER_DIGIT_REGEX, lower=False)
+
+    def test_digit_false(self):
+        for length in self.valid_lengths:
+            self._string_lengths(
+                length, regex=self.UPPER_LOWER_NODIGIT_REGEX, digit=False)
+
+    def test_upper_lower_false(self):
+        for length in self.valid_lengths:
+            self._string_lengths(
+                length, regex=self.NOUPPER_NOLOWER_DIGIT_REGEX, upper=False, lower=False)
+
+    def test_upper_digit_false(self):
+        for length in self.valid_lengths:
+            self._string_lengths(
+                length, regex=self.NOUPPER_LOWER_NODIGIT_REGEX, upper=False, digit=False)
+
+    def test_lower_digit_false(self):
+        for length in self.valid_lengths:
+            self._string_lengths(
+                length, regex=self.UPPER_NOLOWER_NODIGIT_REGEX, lower=False, digit=False)
 
 
 if __name__ == "__main__":
