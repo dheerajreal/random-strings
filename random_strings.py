2a10b44bf91734742191087c4f59cb567bdb89c2
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2021-07-05 17:05:21 +0530

    add get_random_uuid and get_random_hex

--- random_strings.py
+++ random_strings.py
@@ -1,5 +1,8 @@
 import secrets
 import string
+from uuid import uuid4
+
+__all__ = ["get_random_string", "get_random_hex", "get_random_uuid"]
 
 
 def get_random_string(
@@ -37,3 +40,37 @@ def get_random_string(
     if digit:
         chars = chars + string.digits
     return "".join(secrets.choice(chars) for i in range(length))
+
+
+_DEFAULT_ENTROPY = 64
+
+
+def get_random_hex(nbytes: int = None) -> str:
+    """Generate random hexadecimal string suitable for cryptographic use.
+
+    Args:
+        nbytes (int, optional):
+            Number of bytes. Two characters generated for each byte.
+            A suitable default will we used if not given.
+
+    Returns:
+        str: Cryptographically-secure hexadecimal string for given number of bytes of randomness.
+    """
+    if not nbytes:
+        nbytes = _DEFAULT_ENTROPY  # Default
+    return secrets.token_hex(nbytes=nbytes)
+
+
+def get_random_uuid(dashes: bool = True) -> str:
+    """Generate random uuid string suitable for cryptographic use.
+
+    Args:
+        dashes (bool, optional):
+            should dashes be included in the generated string(uuid). Defaults to True.
+
+    Returns:
+        str: String representation of randomly generated uuid
+    """
+    if not dashes:
+        return uuid4().hex
+    return str(uuid4())
