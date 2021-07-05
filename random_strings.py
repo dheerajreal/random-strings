e3a07b68959bcf0a262a5261d99754de688ee946
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2021-07-05 14:28:08 +0530

    added some sanity checks

--- random_strings.py
+++ random_strings.py
@@ -2,7 +2,16 @@ import secrets
 import string
 
 
-def get_random_string(length: int, lower=True, upper=True, digit=True):
+def get_random_string(
+    length: int,
+    lower: bool = True,
+    upper: bool = True,
+    digit: bool = True
+) -> str:
+    if not isinstance(length, int):
+        raise ValueError("length must be an integer")
+    if not length > 0:
+        raise ValueError("length must be greater than zero")
     chars = ""
     if lower:
         chars = chars + string.ascii_lowercase
