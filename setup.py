c1baf2aaa0c00a42b252bdd530af105a1a1e34cb
Dheeraj <44266862+dheerajreal@users.noreply.github.com>
2021-07-18 23:37:56 +0530

    update steup.py with publish

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
+import os
 import pathlib
+import shutil
+import sys
 
-from setuptools import setup
+from setuptools import Command, setup
 
 here = pathlib.Path(__file__).parent.resolve()
 
@@ -8,6 +11,39 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 GITHUB_URL = "https://github.com/dheerajreal/random-strings/"
 VERSION = '0.1.1'
+
+
+class PublishCommand(Command):
+    """Support setup.py publish."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Print things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing older builds")
+            shutil.rmtree(os.path.join(here, "dist"))
+        except FileNotFoundError:
+            print("No older builds exist, Do nothing")
+
+        self.status("Building Source and Wheel")
+        os.system("{0} setup.py sdist bdist_wheel".format(sys.executable))
+
+        self.status("Uploading to PyPi using Twine")
+        os.system("twine upload dist/*")
+
+        sys.exit()
 
 
 setup(
@@ -39,5 +75,9 @@ setup(
     project_urls={
         'Bug Reports': GITHUB_URL + 'issues/',
         'Source': GITHUB_URL,
+    },
+    # setup.py publish
+    cmdclass={
+        "publish": PublishCommand,
     },
 )
